9c7db59014b13532d487fe7dbbd13dbffb35022f
Roberto <robertsantos.dom@gmail.com>
2023-12-30 22:07:42 +0800

    finished

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,6 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    const reversed = string.toLowerCase().replace(/[^a-zA-Z0-9]/g, "");
+    return reversed.split("").reverse().join("") === reversed;
 };
 
 // Do not edit below this line
--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
+const fibonacci = num => {
+    num = Number(num);
+    if (num > 0) {
+        let arr = [];
+        let previousNumber = 0;
+        let currentNumber = 1;
 
+        for (let i = 0; i < num; i++) {
+            arr.push(currentNumber);
+            [currentNumber, previousNumber] = [currentNumber + previousNumber, currentNumber];
+        }
+
+        return arr[arr.length - 1];
+    } else {
+        return "OOPS";
+    }
 };
 
 // Do not edit below this line
