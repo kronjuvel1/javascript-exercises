192b383604f30d87759be96ce73d34d67e007f92
Roberto <robertsantos.dom@gmail.com>
2023-12-30 23:50:48 +0800

    finished

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,18 @@
-const getTheTitles = function() {
+const books = [
+    {
+      title: 'Book',
+      author: 'Name'
+    },
+    {
+      title: 'Book2',
+      author: 'Name2'
+    }
+  ]
 
+const getTheTitles = function() {
+    const bookTitle = books.map(obj => obj.title);
+
+    return bookTitle;
 };
 
 // Do not edit below this line
