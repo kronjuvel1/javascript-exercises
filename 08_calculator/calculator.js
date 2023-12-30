b5c2d15a87ef9f235efba95ad13873a2c4d7fbf4
Roberto <robertsantos.dom@gmail.com>
2023-12-30 18:09:08 +0800

    finished

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,39 @@
-const add = function() {
-	
+const add = function (a, b) {
+  return a + b
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (arr) {
+  let sum = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
+
+  return sum;
 };
 
-const multiply = function() {
+const multiply = function (array) {
+  return array.reduce((product, current) => product * current);
+}
 
+const power = function (a, b) {
+  return Math.pow(a, b);
 };
 
-const power = function() {
-	
-};
+const factorial = function (n) {
+  if (n === 0) return 1;
+  
+  let result = 1;
+  for (let i = 2; i <= n; i++) {
+    result *= i;
+  }
 
-const factorial = function() {
-	
-};
+  return result;
+}
 
 // Do not edit below this line
 module.exports = {
